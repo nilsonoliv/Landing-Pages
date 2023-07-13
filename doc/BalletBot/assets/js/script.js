9a5f6489abc4f57360822f48dec88150a6d5215b
nilsonoliv <nilsonespiritosant@gmail.com>
2023-07-13 09:35:03 -0300

    clear js

--- doc/BalletBot/assets/js/script.js
+++ doc/BalletBot/assets/js/script.js
@@ -1,26 +1,3 @@
-/*function abrirmenu(){
-    let BarraMenuAberto = document.getElementById ('.navegacao'); 
-    let BarraMenu = document.getElementById ('.icon-menu'); 
-
-    if (BarraMenuAberto.style.display == "none" && BarraMenu.style.display == "block"){
-        BarraMenuAberto.style.display = "flex"; 
-        BarraMenu.style.display = "none"; 
-    }else{
-        BarraMenuAberto.style.display = "none";
-        BarraMenu.style.display = "block";
-    }
-}
-
-*/
-/*
-document.addEventListener('DOMContentLoaded', function(){
-    function abrirmenu(){
-        let BarraMenuAberto = document.getElementById ('.cabecalho'); 
-        console.log('ok funciona a função');
-        BarraMenuAberto.style.backgroundColor = "transparent";
-    }
-})*/
-
 function abrirmenu(){
     let BarraMenuAberto = document.querySelector('.navegacao'); 
     let BarraMenu = document.querySelector('.icon-menu'); 
