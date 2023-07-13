e0b488372aa1f54202fac096e7456951874295e2
nilsonoliv <nilsonespiritosant@gmail.com>
2023-07-13 09:34:23 -0300

    add menu close

--- doc/BalletBot/assets/js/script.js
+++ doc/BalletBot/assets/js/script.js
@@ -22,7 +22,27 @@ document.addEventListener('DOMContentLoaded', function(){
 })*/
 
 function abrirmenu(){
-    let BarraMenuAberto = document.querySelector('.cabecalho'); 
-    console.log('ok funciona a função');
-    BarraMenuAberto.style.padding = "200px";
+    let BarraMenuAberto = document.querySelector('.navegacao'); 
+    let BarraMenu = document.querySelector('.icon-menu'); 
+
+    if (BarraMenuAberto.style.display == "none" && BarraMenu.style.display == "block"){
+        BarraMenuAberto.style.display = "flex"; 
+        BarraMenu.style.display = "none"; 
+    }else{
+        BarraMenuAberto.style.display = "none";
+        BarraMenu.style.display = "block";
+    }    
+}
+
+function closeMenu(){
+    let BarraMenuAberto = document.querySelector('.navegacao'); 
+    let BarraMenu = document.querySelector('.icon-menu'); 
+
+    if (BarraMenuAberto.style.display == "flex" && BarraMenu.style.display == "none"){
+        BarraMenuAberto.style.display = "none"; 
+        BarraMenu.style.display = "block"; 
+    }else{
+        BarraMenuAberto.style.display = "flex";
+        BarraMenu.style.display = "none";
+    }    
 }
